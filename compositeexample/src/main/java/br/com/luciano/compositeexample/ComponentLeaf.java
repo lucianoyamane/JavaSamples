76b34df6846b3ed9896e895b299ed5bdf19f022a
LAYO <lucianoaugusto@gmail.com>
2017-09-11 20:39:33 -0300

    Codigo inicial

--- compositeexample/src/main/java/br/com/luciano/compositeexample/ComponentLeaf.java
+++ compositeexample/src/main/java/br/com/luciano/compositeexample/ComponentLeaf.java
@@ -2,8 +2,22 @@ package br.com.luciano.compositeexample;
 
 public class ComponentLeaf implements Component {
 
+    private String leafText;
+
     public void desenhar(String text) {
-        System.out.print(" Leaf " + text + " \n");
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append(" Leaf ").append(text);
+        if (leafText != null) {
+            stringBuilder.append(" ").append(leafText).append(" ");
+        }
+        stringBuilder.append(" \n");
+
+        System.out.print(stringBuilder.toString());
+    }
+
+    public final ComponentLeaf addLeafText(String leafText) {
+        this.leafText = leafText;
+        return this;
     }
 
     private ComponentLeaf() {
--- compositeexample/src/main/java/br/com/luciano/compositeexample/Test.java
+++ compositeexample/src/main/java/br/com/luciano/compositeexample/Test.java
@@ -4,8 +4,8 @@ public class Test {
 
     public static void main(String[] args) {
         ComponentComposite componentComposite = ComponentComposite.init();
-        componentComposite.addComponent(ComponentLeaf.init());
-        componentComposite.addComponent(ComponentLeaf.init());
+        componentComposite.addComponent(ComponentLeaf.init().addLeafText("first"));
+        componentComposite.addComponent(ComponentLeaf.init().addLeafText("second"));
         componentComposite.addComponent(ComponentLeaf.init());
 
         ComponentComposite finalComponent = ComponentComposite.init();
